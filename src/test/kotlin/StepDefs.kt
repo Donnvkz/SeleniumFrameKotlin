4c61f03804dde0bd150362854f08b919fabb1ede
Don <donnvkz@ya.ru>
2018-11-17 14:48:39 +0700

    version2

---
+++ src/test/kotlin/StepDefs.kt
@@ -0,0 +1,45 @@
+
+import cucumber.api.java.After
+import cucumber.api.java.Before
+import cucumber.api.java8.En
+import initUtil.UtilResources
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import java.util.concurrent.TimeUnit
+
+class StepDefs: En {
+    lateinit var driver: WebDriver
+        private set
+
+    @Before
+    fun setUp() {
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe")
+        driver = ChromeDriver()
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+        driver.manage().window().maximize()
+        driver.get(UtilResources.getParam("pageURL"))
+    }
+
+    @After
+    fun driverClose() {
+        driver.close();
+    }
+
+    init {
+        Given("^I have (\\d+) cukes in my belly$") { cukes: Int ->
+            print(cukes)
+        }
+
+        When("^I wait (\\d+) hour$") { arg1: Int ->
+            print(arg1)
+//            // Write code here that turns the phrase above into concrete actions
+//            throw PendingException()
+        }
+
+        Then("^my belly should growl$") {
+            //            // Write code here that turns the phrase above into concrete actions
+//            throw PendingException()
+        }
+    }
+}
